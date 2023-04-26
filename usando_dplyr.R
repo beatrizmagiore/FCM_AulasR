7a43f58360cae1947468e48ca6de30ce92bad404
beatrizmagiore <beatriz.magiore@unesp.br>
2023-04-26 12:58:33 -0300

    pré-aula 27/04

--- usando_dplyr.R
+++ usando_dplyr.R
@@ -143,3 +143,204 @@ dados %>%
   ) %>% 
   arrange(type)
 #######################################################
+
+#TODO as variáveis quantitativas
+
+
+#? Renomear colunas
+dados %>%
+  group_by(type) %>%
+  summarise(
+    media_altura = mean(height),
+    media_peso = mean(weight),
+    N = n()
+  ) %>%
+  arrange(media_altura) %>%
+  rename("Número de pokemons" = N)
+
+#? Movê-las
+dados %>%
+  group_by(type) %>%
+  summarise(
+    media_altura = mean(height),
+    media_peso = mean(weight),
+    N = n()
+  ) %>%
+  arrange(media_altura) %>%
+  rename("Número de pokemons" = N) %>%
+  relocate("Número de pokemons")
+dados %>%
+  group_by(type) %>%
+  summarise(
+    media_altura = mean(height),
+    media_peso = mean(weight),
+    N = n()
+  ) %>%
+  arrange(media_altura) %>%
+  rename("Número de pokemons" = N) %>%
+  relocate("Número de pokemons", .after = type)
+
+
+#? rowwise: linha por linha
+
+#? A função mutate e outras do pacote dplyr trabalham diretamente com as colunas
+#? como se fossem operações de vetor
+
+dados %>%
+  mutate(
+    name2 = paste(name, " - NOVO")
+  ) %>% head
+
+#? Na prática, o dplyr faz isso:
+paste(dados$name, "- NOVO")
+
+#? o que significa que a função PRECISA aceitar um vetor
+
+#? Imagine que você queira criar uma função que testa se o valor de uma coluna
+#? na observação i é maior ou menor que um dado valor e executa uma certa ação.
+
+f <- function(x){
+  if(x <= 15){ #? no caso, o valor é 300
+    return("Executei essa ação")
+  }else{
+    
+    return("Executei Aquela ação")
+  }
+}
+
+#! O código abaixo não funciona
+dados %>%
+  mutate(
+    nova_var = f(height)
+  ) %>%
+  select(height, nova_var) %>% head(30)
+
+#* O código abaixo funciona
+#TODO
+dados %>%
+  rowise() %>% 
+  mutate(
+    nova_var = f(height)
+  ) %>%
+  select(height, nova_var) %>% head(30)
+
+#? ifelse e case_when
+
+dados %>%
+  mutate(
+    tamanho = ifelse(
+      height < 15,
+      "baixinho",
+      "altão"
+    )
+  ) %>% head
+
+dados %>%
+  mutate(
+    tamanho = case_when(
+      height < 5 ~ "baixinho",
+      height < 10 ~ "pequeno",
+      height < 15 ~ "médio",
+      TRUE ~ "altão"
+    )
+  ) %>% head
+
+#? Alguns perrengues da vida
+
+#! O código abaixo não funciona
+dados %>%
+  mutate(
+    tamanho = case_when(
+      height < 5 ~ "baixinho",
+      height < 10 ~ "pequeno",
+      height < 15 ~ NA,
+      TRUE ~ "altão"
+    )
+  ) %>% head
+
+#* O código abaixo conserta isso
+#TODO
+
+
+#? Vamos falar de JOIN
+
+df_means <- dados %>%
+  group_by(type) %>%
+  summarise(
+    media_h = mean(height),
+    media_w = mean(weight)
+  )
+df_means
+
+#? vamos excluir os grupos que começam com "g"
+#TODO
+
+#? vamos adicionar um grupo que não existe
+
+novo_grupo <- data.frame(
+  type = "Vozes da minha cabeça",
+  media_h = 1000,
+  media_w = 400.82
+)
+
+#TODO adicionar o grupo
+
+#? full_join
+#TODO
+
+#? inner_join
+#TODO
+
+#? left_join
+#TODO
+
+#? right_join
+#TODO
+
+
+
+#? vamos adicionar um grupo que JÁ existe
+
+novo_grupo <- data.frame(
+  type = "bug",
+  media_h = 10,
+  media_w = 800
+)
+
+#TODO adicionar o grupo
+
+#? left_join
+#TODO
+
+#? right_join
+#TODO
+
+
+#?#####################################################################
+#? TIDYR
+#?#####################################################################
+
+library(tidyr)
+
+#? baixado de https://livro.curso-r.com/
+
+dados <- readr::read_rds("Dados/imdb.rds")
+
+head(dados)
+names(dados)
+
+#TODO checar se cada filme tem apenas um genero associado
+
+#? Pivoteamento
+
+#? pivot_wider
+
+#? pivot_longer
+
+
+#? Emissões de ar
+#? https://www.kaggle.com/datasets/ashishraut64/global-methane-emissions
+
+
+#? Netflix
+#? https://www.kaggle.com/datasets/victorsoeiro/netflix-tv-shows-and-movies
